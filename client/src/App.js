91a92d0c3763851f29f90514cee8810600ca4464
Fredes Virginia <94409602+FredesVirginia@users.noreply.github.com>
2023-11-06 17:30:19 +0000

    nuevo

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Route} from 'react-router-dom';
-import LadinPage from "./Componets/LadinPage/LadinPage";
-import DriverContainer from "./Componets/Home/DriverContainer";
-import DriverId from "./Componets/DriverId/DriverId";
-import Form from "./Componets/CreateDriver/Form";
-import UpdateDriver from './Componets/UpdateDriver/UpdateDriver';
+import Login from "../src/Components/Login";
+import Register from './Components/Register';
+import Dashboard from './Components/Dashboard';
 import "./App.css";
-import axios from "axios";
-axios.defaults.baseURL = "http://localhost:3001/";
+
 const App = () => {
     return (
         <div >
             <BrowserRouter>
-      <Route exact path='/' component={LadinPage}/>
-      <Route exact path='/drivers' component={DriverContainer}/>
-      <Route exact path='/createDriver' component={Form}/>
-      <Route exact path='/drivers/:id' component={DriverId}/>
-      <Route exact path='/drivers/editar/:id' component={UpdateDriver}/>
+      <Route exact path='/' component={Login}/>
+      <Route exact path='/drivers' component={Register}/>
+      <Route exact path='/createDriver' component={Dashboard}/>
+    
         
       </BrowserRouter>
         </div>
